0814c6a2480120b927193a40e9166a078a70358b
lenlordcode <115409863+lenlordcode@users.noreply.github.com>
2023-12-19 02:13:57 +0300

    edit vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/newdisk-vue-project/',
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/newdisk-vue-project/'
+      : '/',
+  devServer: {
+    historyApiFallback: true,
+  },
 })
